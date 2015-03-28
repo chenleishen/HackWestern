93e7ae04337483171b8aa8e305e8a3a3b837805e
hsennik <hsennik@uwaterloo.ca>
2015-03-28 17:01:46 -0400

    got the third page working
    page for selecting twinkle twinkle

--- AmericanSingLanguage/app/src/main/java/hannahmeerajoeshelley/americansinglanguage/SecondPage.java
+++ AmericanSingLanguage/app/src/main/java/hannahmeerajoeshelley/americansinglanguage/SecondPage.java
@@ -4,18 +4,29 @@
 package hannahmeerajoeshelley.americansinglanguage;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
-    public class SecondPage extends Activity {
+    public class SecondPage extends Activity implements View.OnClickListener {
 
-        ImageButton button;
+        ImageButton listbutton;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
+            listbutton = (ImageButton) findViewById(R.id.ChooseFromListButton);
+            listbutton.setOnClickListener(this);
+        }
+
+        public void onClick (View v){
+            final Context context = this;
+            Intent intent = new Intent(context, ThirdPage.class);
+            startActivity(intent);
         }
 
     }
---
+++ AmericanSingLanguage/app/src/main/java/hannahmeerajoeshelley/americansinglanguage/ThirdPage.java
@@ -0,0 +1,28 @@
+package hannahmeerajoeshelley.americansinglanguage;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import hannahmeerajoeshelley.americansinglanguage.R;
+
+        import android.app.Activity;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.ImageButton;
+
+public class ThirdPage extends Activity {
+
+    //ImageButton twinklebutton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_from_list);
+
+        //twinklebutton = (ImageButton) findViewById(R.id.);
+        //twinklebutton.setOnClickListener(this);
+    }
+}
