0911abefab962fcdd75be78a596dd1a9fd7e0f68
hsennik <hsennik@uwaterloo.ca>
2015-03-28 17:32:25 -0400

    get to fourth page
    go to fourth page when you select twinkle radio button

--- AmericanSingLanguage/app/src/main/java/hannahmeerajoeshelley/americansinglanguage/ThirdPage.java
+++ AmericanSingLanguage/app/src/main/java/hannahmeerajoeshelley/americansinglanguage/ThirdPage.java
@@ -1,28 +1,29 @@
 package hannahmeerajoeshelley.americansinglanguage;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ImageButton;
+ import android.app.Activity;
+ import android.content.Context;
+ import android.content.Intent;
+ import android.os.Bundle;
+ import android.view.View;
+ import android.widget.RadioButton;
 
-import hannahmeerajoeshelley.americansinglanguage.R;
+public class ThirdPage extends Activity implements View.OnClickListener {
 
-        import android.app.Activity;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.ImageButton;
-
-public class ThirdPage extends Activity {
-
-    //ImageButton twinklebutton;
+    RadioButton twinklebutton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_from_list);
 
-        //twinklebutton = (ImageButton) findViewById(R.id.);
-        //twinklebutton.setOnClickListener(this);
+        twinklebutton = (RadioButton) findViewById(R.id.radioButton);
+        twinklebutton.setOnClickListener(this);
     }
-}
+
+        public void onClick (View v){
+            final Context context = this;
+            Intent intent = new Intent(context, FourthPage.class);
+            startActivity(intent);
+        }
+
+    }
