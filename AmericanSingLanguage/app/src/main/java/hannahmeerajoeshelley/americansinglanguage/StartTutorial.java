df1644a1efbcdf02c01d3ffde821af916b09d618
chenleishen <shenchenlei@hotmail.com>
2015-03-29 00:59:24 -0400

    trying to play videos in loop
    pushing change for others to branch off so they can work on it as well

--- AmericanSingLanguage/app/src/main/java/hannahmeerajoeshelley/americansinglanguage/StartTutorial.java
+++ AmericanSingLanguage/app/src/main/java/hannahmeerajoeshelley/americansinglanguage/StartTutorial.java
@@ -1,10 +1,10 @@
 package hannahmeerajoeshelley.americansinglanguage;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 
@@ -15,8 +15,8 @@ import android.widget.VideoView;
 public class StartTutorial extends Activity {
 
     private VideoView myVideoView;
-    private int position = 0;
-    private ProgressDialog progressDialog;
+    //private int position = 0;
+    //private ProgressDialog progressDialog;
     private MediaPlayer mediaPlay;
     private String word="twinkle";
 
@@ -26,6 +26,33 @@ public class StartTutorial extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.asl_tutorial_page);
 
+        TextView titleBox = (TextView) findViewById(R.id.TutorialTitle);
+        String mary = getResources().getString(R.string.my_big_string);
+        String[] words = mary.split("");
+
+        playWord();
+
+        MediaPlayer.OnCompletionListener completionListener=new MediaPlayer.OnCompletionListener() {
+
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                playWord();
+            }
+        };
+
+//        int i = 0;
+//        int size = words.length;
+//
+//        while(i < size) {
+//                word = words[i];
+//                titleBox.setText(word);
+//                //playWord(word);
+//                //i+=1;
+//        }
+
+    }
+
+    public void playWord(String word) {
         if (mediaPlay == null) {
             mediaPlay = MediaPlayer.create(this,R.raw.twinkle);
         }
@@ -34,40 +61,7 @@ public class StartTutorial extends Activity {
         myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.twinkle));
         myVideoView.start();
 
-//        progressDialog = new ProgressDialog(StartTutorial.this);
-//        progressDialog.show();
-
-//        try {
-//            //set the media controller in the VideoView
-//            myVideoView.setMediaPlayer(mediaPlay);
-//            //set the uri of the video to be played
-//            myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.twinkle));
-//        } catch (Exception e) {
-//            Log.e("Error", e.getMessage());
-//            e.printStackTrace();
-//        }
-
-//        myVideoView.requestFocus();
-//        //we also set an setOnPreparedListener in order to know when the video file is ready for playback
-//        myVideoView.setOnPreparedListener(new OnPreparedListener() {
-//            public void onPrepared(MediaPlayer mediaPlayer) {
-//                // close the progress bar and play the video
-//                progressDialog.dismiss();
-//                //if we have a position on savedInstanceState, the video playback should start from here
-//                myVideoView.seekTo(position);
-//                if (position == 0) {
-//                    myVideoView.start();
-//                } else {
-//                    //if we come from a resumed activity, video playback will be paused
-//                    myVideoView.pause();
-//                }
-//            }
-//        });
-
-
-        //String mary = getResources().getString(R.string.my_big_string);
-        //String[] words = mary.split("");
-
     }
+
 
 }
