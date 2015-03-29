b77d968d6aa50bd9853b21931dc024ed4a4bd27e
meerakanani <mkanani@uwaterloo.ca>
2015-03-29 00:59:35 -0400

    Revert "Revert "tutorial pages""
    This reverts commit 3429af9c6d67b3433a94ac187b158e414aa7da3f.

--- AmericanSingLanguage/app/src/main/java/hannahmeerajoeshelley/americansinglanguage/BeforeTutorialTwinkle.java
+++ AmericanSingLanguage/app/src/main/java/hannahmeerajoeshelley/americansinglanguage/BeforeTutorialTwinkle.java
@@ -1,16 +1,20 @@
 package hannahmeerajoeshelley.americansinglanguage;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
 
 
 public class BeforeTutorialTwinkle extends Activity implements View.OnClickListener {
 
     ImageButton signaway;
+    RadioButton radioButton1, radioButton2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,12 +23,33 @@ public class BeforeTutorialTwinkle extends Activity implements View.OnClickListe
 
         signaway = (ImageButton) findViewById(R.id.sign);
         signaway.setOnClickListener(this);
-
+        radioButton1 = (RadioButton) findViewById(R.id.radioButton1);
+        radioButton2 = (RadioButton) findViewById(R.id.radioButton2);
     }
     public void onClick (View v){
         final Context context = this;
-        Intent intent = new Intent(context, StartTutorial.class);
-        startActivity(intent);
+        Intent intent =  null;
+        if(radioButton1.isChecked() || radioButton2.isChecked()) {
+            intent = new Intent(context, StartTutorial.class);
+            startActivity(intent);
+        }
+        // prompt and refresh page
+        else {
+            /*new AlertDialog.Builder(this)
+                    .setTitle("Error")
+                    .setMessage("Remember to select a radio box first")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //continue
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert).show();*/
+            //intent = new Intent(context, BeforeTutorialTwinkle.class);
+            finish();
+            startActivity(getIntent());
+        }
+
     }
 
 }
